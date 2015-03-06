4e6666d9ae53262c580c9100f0e96c2ca86f22cf
Carter Roughton <carter.roughton@tstllc.net>
2015-03-06 16:34:15 -0500

    Plot1

--- plot1.R
+++ plot1.R
@@ -5,4 +5,6 @@ setwd(".")
 DT <- read.csv("./dataset/household_power_consumption.txt", sep=";", head=F, skip=66637, nrows=2979, na.strings=c("?"), strip.white=T)
 DT<- rename(DT,date=V1, time=V2, global_active_power=V3,global_reactive_power=V4,voltage=V5,global_intensity=V6,sub_metering_1=V7,sub_metering_2=V8,sub_metering_3=V9)
 DT[,1] <- dmy(DT[,1])
-hist
+hist(DT$global_active_power,col="red",xlab="Global Active Power",main="Global Active Power")
+dev.copy(png,file="plot1.png",width=480,height=480)
+dev.off()
