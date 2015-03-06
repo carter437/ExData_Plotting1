4b18d5ad9506916773a5f658d198dc3e08e384f8
Carter Roughton <carter.roughton@tstllc.net>
2015-03-06 16:42:47 -0500

    Create readDataSets.R to abstract away reading in datasets

--- plot1.R
+++ plot1.R
@@ -1,10 +1,7 @@
-library("lubridate")
-library("dplyr")
 library("datasets")
-setwd(".")
-DT <- read.csv("./dataset/household_power_consumption.txt", sep=";", head=F, skip=66637, nrows=2979, na.strings=c("?"), strip.white=T)
-DT<- rename(DT,date=V1, time=V2, global_active_power=V3,global_reactive_power=V4,voltage=V5,global_intensity=V6,sub_metering_1=V7,sub_metering_2=V8,sub_metering_3=V9)
-DT[,1] <- dmy(DT[,1])
+source("readDataSet.R")
+
+DT <- readDataSet()
 hist(DT$global_active_power,col="red",xlab="Global Active Power",main="Global Active Power")
 dev.copy(png,file="plot1.png",width=480,height=480)
 dev.off()
