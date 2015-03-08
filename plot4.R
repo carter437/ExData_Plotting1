914da3e3782ed41e57c05ade2f288b87f92ba418
Carter Roughton <carter.roughton@tstllc.net>
2015-03-07 21:30:06 -0500

    Adjusting labels

--- plot4.R
+++ plot4.R
@@ -27,15 +27,15 @@ plot3 <- function(){
 
 voltagePlot <- function(){
   DT <- readDataSet()
-  plot(DT$voltage,type="l",xlab="        datetime",ylab="Voltage", frame.plot=T, xaxt="n")
-  axis(1, at=c(0, 1500, 2880), main="datetime", labels=c("Thur","Fri","Sat"))
+  plot(DT$voltage,type="l",xlab="   datetime",ylab="Voltage", frame.plot=T, xaxt="n")
+  axis(1, at=c(0, 1500, 2880), labels=c("Thur","Fri","Sat"))
   axis(2, at=c(0,2,4,6))
 }
 
 globalReactivePowerPlot <- function(){
   DT <- readDataSet()
-  plot(DT$global_reactive_power,type="l",xlab="       datetime",ylab="Global_reactive_power", frame.plot=T, xaxt="n")
-  axis(1, at=c(0, 1500, 2880), main="datetime", labels=c("Thur","Fri","Sat"))
+  plot(DT$global_reactive_power,type="l",xlab="  datetime",ylab="Global_reactive_power", frame.plot=T, xaxt="n")
+  axis(1, at=c(0, 1500, 2880), labels=c("Thur","Fri","Sat"))
 }
 
 par(mfrow = c(2, 2))
